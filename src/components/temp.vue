139327a1bf463133eb6ca3bdabaa7cbf3560789e
MajorDong <odsts@outlook.com>
2019-11-06 19:19:05 +0800

    first commit

---
+++ src/components/temp.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'temp',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
